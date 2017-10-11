d83e20997bd9aa54c6a7b3276e70e9984e0b37e3
dgisolfi <daniel.gisolfi1@marist.edu>
2017-10-10 20:03:59 -0400

    Finished assigment 2

--- assignments/assignment2/Limbo.sql
+++ assignments/assignment2/Limbo.sql
@@ -1,54 +1,55 @@
-# limbo.sql
-# Purpose: Creates and populates the limbo database
-# Authors: James Ekstract, Daniel Gisolfi
-# Version 0.1
- 
-# Create and use limbo_db database
+#Limbo.sql
+#Create and populate a database for Limbo
+#Authors: James Ekstract, Daniel Gisolfi
+#Version 0.1
+
 CREATE DATABASE IF NOT EXISTS limbo_db;
 USE limbo_db;
- 
-# Create users table
+
 CREATE TABLE IF NOT EXISTS users(
-        user_id     INT     AUTO_INCREMENT PRIMARY KEY, 
-        first_name  TEXT, 
-        last_name   TEXT,
-        email       TEXT,
-        pass        TEXT,
-        reg_date    DATETIME);
- 
-# Populate users table
-INSERT INTO users (first_name, pass)
-VALUES ("admin", "gaze11e");
- 
-# Create stuff table
+	user_id 	INT 	AUTO_INCREMENT 	PRIMARY KEY,
+	first_name 	text,
+	last_name 	text,
+	email 		text,
+	pass 		text,
+	reg_date 	DATETIME);
+
+INSERT INTO USERS(first_name, pass)
+VALUES("admin", "gaze11e");
+
+
 CREATE TABLE IF NOT EXISTS stuff(
-        id          INT                             AUTO_INCREMENT PRIMARY KEY, 
-        location_id INT                             NOT NULL,
-        description TEXT                            NOT NULL,
-        create_date DATETIME                        NOT NULL,
-        update_date DATETIME                        NOT NULL,
-        room        TEXT,
-        owner       TEXT,
-        finder      TEXT,
-        status      SET('found', 'lost', 'claimed') NOT NULL);
- 
-# Create locations table
+	id 			INT AUTO_INCREMENT 	PRIMARY KEY,
+	location_id INT 		NOT NULL,
+	description TEXT 		NOT NULL,
+	create_date DATETIME 	NOT NULL,
+	update_date DATETIME 	NOT NULL,
+	room 		TEXT,
+	owner 		TEXT,
+	finder 		TEXT,
+	status 		SET  ('found','lost', 'claimed') NOT NULL);
+
+INSERT INTO USERS(first_name, pass)
+VALUES("admin", "gaze11e");
+
+
 CREATE TABLE IF NOT EXISTS locations(
-        id          INT         AUTO_INCREMENT PRIMARY KEY,
-        create_date DATETIME    NOT NULL,
-        update_date DATETIME    NOT NULL,
-        name        TEXT        NOT NULL);
- 
-# Populate locations table
+	id 			INT 	 AUTO_INCREMENT 	PRIMARY KEY,
+	create_date DATETIME NOT NULL,
+	update_date DATETIME NOT NULL,
+	name 		TEXT 	 NOT NULL);
+
 INSERT INTO locations(create_date, update_date, name)
 VALUES(Now(), Now(), "Donnelly"),
-      (Now(), Now(), "Hancock"),
-      (Now(), Now(), "Dyson"),
-      (Now(), Now(), "Library"),
-      (Now(), Now(), "McCann"),
-      (Now(), Now(), "Student Center"),
-      (Now(), Now(), "Allied Health"),
-      (Now(), Now(), "Lowell thomas"),
-      (Now(), Now(), "Fontaine"),
-      (Now(), Now(), "Steel Plant"),
-      (Now(), Now(), "51 Fulton");
+	  (Now(), Now(), "Hancock"),
+	  (Now(), Now(), "Dyson"),
+	  (Now(), Now(), "Library"),
+	  (Now(), Now(), "McCann"),
+	  (Now(), Now(), "Student Center"),
+	  (Now(), Now(), "Allied Health"),
+	  (Now(), Now(), "Lowell Thomas"),
+	  (Now(), Now(), "Fontain"),
+	  (Now(), Now(), "Steel Plant"),
+	  (Now(), Now(), "51 Fulton");
+
+SELECT * FROM locations, stuff, users;
