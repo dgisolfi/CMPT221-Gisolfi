6271a99bd65a8cde1f38b4a5e2ac0b9c944b439b
dgisolfi <daniel.gisolfi1@marist.edu>
2017-10-19 09:26:13 -0400

    Started Lab 8

---
+++ Lab8/Lab8Gisolfi.md
@@ -0,0 +1,36 @@
+# LAB 8 CMPT 221
+
+## Daniel Gisolfi & James Ekstract
+
+###Modify stickypresidents.php to incorporate stickiness using either method **A **or method **B**.
+
+**stickypresidents.php**
+
+```php
+
+```
+
+### Before submitting a new president
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/a.png" width="300px" />
+
+### After submitting a new _Valid_ president
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/b.png" width="300px" />
+
+### Submit a president with no input
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/c.png" width="300px" />
+
+### Submit a president without a number
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/d.png" width="300px" />
+
+### Submit a president without a first name
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/E.png" width="300px" />
+
+### Submit a president without a last name
+
+<img src="file:///Users/daniel/code-repos/CMPT221-Gisolfi/Lab7/images/f.png" width="300px" />
+
---
+++ Lab8/localhost/includes/helpers.php
@@ -0,0 +1,88 @@
+<?php
+$debug = true;
+
+# Shows the records in prints
+function show_records($dbc) {
+	# Create a query to get the name and price sorted by price
+	$query = 'SELECT num, fname, lname FROM presidents ORDER BY num ASC' ;
+
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>' ;
+  		echo '<TABLE border>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+  		echo '<TH>First Name</TH>';
+      echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+    		echo '<TR>' ;
+    		echo '<TD>' . $row['num'] . '</TD>' ;
+    		echo '<TD>' . $row['fname'] . '</TD>' ;
+        echo '<TD>' . $row['lname'] . '</TD>';
+    		echo '</TR>' ;
+  		}
+
+  		# End the table
+  		echo '</TABLE>';
+
+  		# Free up the results in memory
+  		mysqli_free_result( $results ) ;
+	}
+}
+
+# Inserts a record into the prints table
+function insert_record($dbc, $num, $fname, $lname) {
+  $query = 'INSERT INTO presidents(num, fname, lname) VALUES (' . $num . ' , "' . $fname . '", "' . $lname . '" )' ;
+  show_query($query);
+
+  $results = mysqli_query($dbc,$query) ;
+  check_results($results) ;
+
+  return $results ;
+}
+
+# Shows the query as a debugging aid
+function show_query($query) {
+  global $debug;
+
+  if($debug)
+    echo "<p>Query = $query</p>" ;
+}
+
+function valid_number($num) {
+  if(empty($num) || !is_numeric($num)){
+    return false ;   
+  } else {
+    $num = intval($num) ;      
+    if($num <= 0)         
+      return false ;
+  }
+  return true ; 
+}
+
+function valid_name($input) {
+  if(empty($input) || !is_string($input)){
+    return false ;   
+  }
+  return true ; 
+}
+
+# Checks the query results as a debugging aid
+function check_results($results) {
+  global $dbc;
+
+  if($results != true)
+    echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
+}
+?>
