987d78949fc528f3f4693dc07e5552684b976f5c
dgisolfi <daniel.gisolfi1@marist.edu>
2017-11-10 08:29:54 -0500

    Apparanlty when I was drunk i wrote this mess of a function to insert records into the database

---
+++ assignments/assignment4/localhost/scripts/inputRecord.php
@@ -0,0 +1,24 @@
+<?php
+# input.php
+
+require( 'connect_db.php' )
+
+if (isset($_POST['submit'])) {
+	insert_record($dbc);
+}
+
+function insert_record($dbc){
+	$loc = $_POST['location'];
+	$descrp = $_POST['description'];
+	$date = $_POST['date'];
+	$room = $_POST['room'];
+	$owner = $_POST['owner'];
+	$finder = $_POST['finder'];
+	$status = $_POST['status'];
+
+	$sql = 'INSERT INTO stuff (location_id, description, create_date, room, owner, finder, status) VALUES('$loc', '$descrp', '$date', '$room', '$owner', '$finder', '$status')';
+		
+	$result = mysqli_query( $dbc , $sql );
+	check_results($result);
+
+}
